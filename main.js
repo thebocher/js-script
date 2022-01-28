ad0c9a1fe694b564916fe43ecba3e18824db1b1c
thebocher <qurka.kolika@gmail.com>
2022-01-28 12:47:17 +0200

    .

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+function g(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
 document.body.parentElement.innerHTML = g('https://raw.githubusercontent.com/thebocher/js-script/main/h.html')
